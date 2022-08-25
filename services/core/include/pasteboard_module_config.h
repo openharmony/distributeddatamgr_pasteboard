2eece9c75b7d8659d3ba375a46b3a3744b7d5989
cy7717 <chenyu301@huawei.com>
2022-08-25 16:25:16 +0800

    跨设备剪贴板服务拉起
    Signed-off-by: cy7717 <chenyu301@huawei.com>

---
+++ services/core/include/pasteboard_module_config.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <functional>
+#ifndef PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
+#define PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
+namespace OHOS {
+namespace MiscServices {
+class PasteboardModuleConfig {
+public:
+    using SwitchObserver = std::function<void(bool IsSwitchOn)>;
+    static bool IsDistributedModuleOn();
+    static void WatchDistributedModuleSwitch(SwitchObserver observer);
+
+private:
+    static void NotifySwitchObserver();
+    static SwitchObserver observer_;
+};
+} // namespace MiscServices
+} // namespace OHOS
+#endif //PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
