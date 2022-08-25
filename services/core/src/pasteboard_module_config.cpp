c23175bc209b54cc025e36eb4daaacaa0ec050e0
cy7717 <chenyu301@huawei.com>
2022-08-25 18:04:26 +0800

    mod
    Signed-off-by: cy7717 <chenyu301@huawei.com>

--- services/core/src/pasteboard_module_config.cpp
+++ services/core/src/pasteboard_module_config.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,25 +12,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#ifndef PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
-#define PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
-
-#include <functional>
+#include "pasteboard_module_config.h"
 
 namespace OHOS {
 namespace MiscServices {
-class DistributedModuleConfig {
-public:
-    using Observer = std::function<void(bool isOn)>;
-    static bool IsOn();
-    static void Watch(Observer observer);
-    static void Notify();
 
-private:
-    static Observer observer_;
-    static bool isOn_;
-};
-} // namespace MiscServices
+bool DistributedModuleConfig::IsOn()
+{
+    return false;
+}
+
+void DistributedModuleConfig::Watch(Observer observer)
+{
+}
+
+void DistributedModuleConfig::Notify()
+{
+}
 } // namespace OHOS
-#endif // PASTE_BOARD_DISTRIBUTE_MODULE_CONFIG_H
+} // namespace MiscServices
