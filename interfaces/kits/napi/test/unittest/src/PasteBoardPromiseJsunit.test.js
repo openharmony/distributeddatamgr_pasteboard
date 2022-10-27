b60dc0d42382918075e0c2ee74d968b181f27025
cy7717 <chenyu301@huawei.com>
2022-10-27 14:09:12 +0800

    修复js用例失败
    Signed-off-by: cy7717 <chenyu301@huawei.com>

--- interfaces/kits/napi/test/unittest/src/PasteBoardPromiseJsunit.test.js
+++ interfaces/kits/napi/test/unittest/src/PasteBoardPromiseJsunit.test.js
@@ -1707,7 +1707,7 @@ describe('PasteBoardJSTest', function () {
                             expect(data.getRecordAt(0).mimeType).assertEqual("x".repeat(1024))
                             expect(data.getPrimaryWant().bundleName).assertEqual(wantText.bundleName)
                             expect(data.getRecordAt(253).htmlText).assertEqual(htmlText)
-                            expect(data.getRecordAt(512).plainText).assertEqual(plainText)
+                            expect(data.getRecordAt(511).plainText).assertEqual(plainText)
                             done();
                         })
                     })
