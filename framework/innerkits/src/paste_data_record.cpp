c91878f988d77860a0c118a313141430fb1b691b
zzs110 <zhouzishuai@huawei.com>
2023-02-23 08:09:21 +0000

    update framework/innerkits/src/paste_data_record.cpp.
    Signed-off-by: zzs110 <zhouzishuai@huawei.com>

--- framework/innerkits/src/paste_data_record.cpp
+++ framework/innerkits/src/paste_data_record.cpp
@@ -499,7 +499,7 @@ size_t PasteDataRecord::Count()
 
 std::shared_ptr<PixelMap> PasteDataRecord::Vector2PixelMap(std::vector<std::uint8_t> &value)
 {
-    PASTEBOARD_HILOGI(PASTEBOARD_MODULE_CLIENT, "Vector2PixelMap, size is %{public}u", value.size());
+    PASTEBOARD_HILOGI(PASTEBOARD_MODULE_CLIENT, "Vector2PixelMap, size is %{public}zu", value.size());
     if (value.size() == 0) {
         return nullptr;
     }
