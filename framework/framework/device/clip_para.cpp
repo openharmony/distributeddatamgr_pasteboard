dfdca5021dab07be8edd0a6e533dceaff2995519
zzs110 <zhouzishuai@huawei.com>
2023-08-10 12:16:57 +0800

    BLE蓝牙广播需求
    Signed-off-by: zzs110 <zhouzishuai@huawei.com>

--- framework/framework/device/clip_para.cpp
+++ framework/framework/device/clip_para.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "clip_para.cpp"
+#include "clip_para.h"
 
 #include <chrono>
 #include "pasteboard_hilog.h"
