8044cf500eabe8e0675198f1a14b11521c5b5c9b
任倩 <renqian12@huawei.com>
2024-02-19 07:20:36 +0000

    update services/core/src/pasteboard_service.cpp.
    Signed-off-by: 任倩 <renqian12@huawei.com>

--- services/core/src/pasteboard_service.cpp
+++ services/core/src/pasteboard_service.cpp
@@ -1322,7 +1322,7 @@ bool PasteboardService::SetDistributedData(int32_t user, PasteData &data)
     currentEvent_ = event;
     PASTEBOARD_HILOGD(PASTEBOARD_MODULE_SERVICE, "expiration = %{public}" PRIu64, event.expiration);
     std::thread thread([clipPlugin, event, rawData]() mutable {
-        clipPlugin->SetPasteData(event, rawData);
+        clipPlugin->SetPasteData(event, *rawData);
     });
     thread.detach();
     return true;
