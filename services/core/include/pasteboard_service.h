93be6c3cd2a50591b606a212f29bee1e0d754f7b
zhiyihang <zhiyihang1@huawei.com>
2024-06-08 02:40:41 +0000

    update services/core/include/pasteboard_service.h.
    Signed-off-by: zhiyihang <zhiyihang1@huawei.com>

--- services/core/include/pasteboard_service.h
+++ services/core/include/pasteboard_service.h
@@ -152,10 +152,10 @@ private:
         void Notify(const Event &event, std::shared_ptr<PasteData> data);
         void ClearRemoteDataTask(const Event &event);
         std::shared_ptr<PasteData> WaitRemoteData(const Event &event);
-        std::mutex mutex_;
-        std::map<std::string, std::shared_ptr<TaskContext>> dataTasks_;
     private:
         std::atomic<uint32_t> mapKey_ = 0;
+        std::mutex mutex_;
+        std::map<std::string, std::shared_ptr<TaskContext>> dataTasks_;
     };
 
     struct classcomp {
