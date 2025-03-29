72e343322ebfafda039d8bd14a4eb4ef4caa9ebb
vb6174 <zhangyunhu@huawei.com>
2025-03-28 20:39:50 -0700

    增加pasteboard_delay_getter_stub.cpp UT 增加pasteboard_delay_getter_Proxy.cpp UT
    Signed-off-by: vb6174 <zhangyunhu@huawei.com>

---
+++ services/test/unittest/src/pasteboard_delay_getter_proxy_test.cpp
@@ -0,0 +1,325 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "ipc_skeleton.h"
+#include "message_parcel_warp.h"
+#include "pasteboard_hilog.h"
+#define private public
+#include "pasteboard_delay_getter_proxy.h"
+#undef private
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS::MiscServices;
+namespace OHOS {
+namespace {
+int g_sendRequest = 0;
+class RemoteObjectTest : public IRemoteObject {
+public:
+    explicit RemoteObjectTest(std::u16string descriptor) : IRemoteObject(descriptor) {}
+    ~RemoteObjectTest() {}
+
+    int32_t GetObjectRefCount() { return 0; }
+    int SendRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option) { return g_sendRequest; }
+    bool AddDeathRecipient(const sptr<DeathRecipient> &recipient) { return true; }
+    bool RemoveDeathRecipient(const sptr<DeathRecipient> &recipient) { return true; }
+    int Dump(int fd, const std::vector<std::u16string> &args) { return 0; }
+};
+
+constexpr int64_t TEST_ERROR_PAW_DATA_SIZE = -1;
+constexpr int64_t TEST_MAX_RAW_DATA_SIZE = 256 * 1024 * 1024; // 256M
+constexpr int64_t ERR_INVALID_PARAMETER = 401;
+}//namespace
+
+class PasteboardDelayProxyTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+    void SetSendRequestResult(int status = 0);
+};
+
+void PasteboardDelayProxyTest::SetUpTestCase(void) { }
+
+void PasteboardDelayProxyTest::TearDownTestCase(void) { }
+
+void PasteboardDelayProxyTest::SetUp(void) { }
+
+void PasteboardDelayProxyTest::TearDown(void) { }
+
+void PasteboardDelayProxyTest::SetSendRequestResult(int status)
+{
+    g_sendRequest = status;
+} 
+
+class PasteboardDelayProxyInterface : public MessageParcel{
+public:
+    PasteboardDelayProxyInterface() {};
+    virtual ~PasteboardDelayProxyInterface() {};
+    virtual bool WriteInterfaceToken(std::u16string name) = 0;
+    virtual bool WriteString(const std::string &value) = 0;
+    virtual int SendRequest(uint32_t code, MessageParcel &data,
+                            MessageParcel &reply, MessageOption &option) = 0;
+    virtual int64_t ReadInt64() = 0;
+    virtual const void *ReadRawData(MessageParcel &parcelPata, size_t size) = 0;
+    virtual bool Decode(const std::vector<std::uint8_t> &buffer) = 0;
+    virtual const uint8_t *ReadUnpadBuffer(size_t length) = 0;
+};
+
+class PasteboardDelayProxyInterfaceMock : public PasteboardDelayProxyInterface {
+public:
+    PasteboardDelayProxyInterfaceMock();
+    ~PasteboardDelayProxyInterfaceMock() override;
+    MOCK_METHOD1(WriteInterfaceToken, bool(std::u16string name));
+    MOCK_METHOD1(WriteString, bool(const std::string &value));
+    MOCK_METHOD4(SendRequest, int(uint32_t code, MessageParcel &data,
+                                  MessageParcel &reply, MessageOption &option));
+    MOCK_METHOD0(ReadInt64, int64_t());
+    MOCK_METHOD2(ReadRawData, const void*(MessageParcel &parcelPata, size_t size));
+    MOCK_METHOD1(Decode, bool(const std::vector<std::uint8_t> &buffer));
+    MOCK_METHOD1(ReadUnpadBuffer, const uint8_t *(size_t));
+};
+
+static void *g_interface = nullptr;
+
+PasteboardDelayProxyInterfaceMock::PasteboardDelayProxyInterfaceMock()
+{
+    g_interface = reinterpret_cast<void *>(this);
+}
+
+PasteboardDelayProxyInterfaceMock::~PasteboardDelayProxyInterfaceMock()
+{
+    g_interface = nullptr;
+}
+
+static PasteboardDelayProxyInterface *GetPasteboardDelayProxyInterface()
+{
+    return reinterpret_cast<PasteboardDelayProxyInterface*>(g_interface);
+}
+
+extern "C" {
+    bool MessageParcel::WriteInterfaceToken(std::u16string name)
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return false;
+        }
+        return interface->WriteInterfaceToken(name);
+    }
+
+    bool Parcel::WriteString(const std::string &value)
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return false;
+        }
+        return interface->WriteString(value);
+    }
+
+    int IRemoteObject::SendRequest(uint32_t code, MessageParcel &data,
+                                       MessageParcel &reply, MessageOption &option)
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return -1;
+        }
+        return interface->SendRequest(code, data, reply, option);
+    }
+
+    int64_t Parcel::ReadInt64()
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return -1;
+        }
+        return interface->ReadInt64();
+    }
+
+    bool TLVReadable::Decode(const std::vector<uint8_t> &buffer)
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return false;
+        }
+        return interface->Decode(buffer);
+    }
+
+    const uint8_t *Parcel::ReadUnpadBuffer(size_t length)
+    {
+        PasteboardDelayProxyInterface *interface = GetPasteboardDelayProxyInterface();
+        if (interface == nullptr) {
+            return nullptr;
+        }
+        return interface->ReadUnpadBuffer(length);
+    }
+}
+
+/**
+ * @tc.name: GetPasteDataTest001
+ * @tc.desc: Function GetPasteData when WriteInterfaceToken return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest001, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(false));
+
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest002
+ * @tc.desc: Function GetPasteData when WriteString return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest002, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(false));
+
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest003
+ * @tc.desc: Function GetPasteData when SendRequest return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest003, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(true));
+    SetSendRequestResult(ERR_INVALID_PARAMETER);
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+    SetSendRequestResult(OHOS::ERR_OK);
+}
+
+/**
+ * @tc.name: GetPasteDataTest004
+ * @tc.desc: Function GetPasteData when ReadInt64 return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest004, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, SendRequest(testing::_, testing::_, testing::_, testing::_))
+               .WillRepeatedly(Return(OHOS::ERR_OK));
+    EXPECT_CALL(mock, ReadInt64()).WillRepeatedly(Return(TEST_ERROR_PAW_DATA_SIZE));
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest005
+ * @tc.desc: Function GetPasteData when ReadInt64 return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest005, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, SendRequest(testing::_, testing::_, testing::_, testing::_))
+               .WillRepeatedly(Return(OHOS::ERR_OK));
+    EXPECT_CALL(mock, ReadInt64()).WillRepeatedly(Return(TEST_MAX_RAW_DATA_SIZE));
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest006
+ * @tc.desc: Function GetPasteData when ReadRawData return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest006, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    std::vector<uint8_t> mockData = {0x01, 0x02, 0x03};
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, SendRequest(testing::_, testing::_, testing::_, testing::_))
+               .WillRepeatedly(Return(OHOS::ERR_OK));
+    EXPECT_CALL(mock, ReadInt64()).WillRepeatedly(Return(mockData.size()));
+    EXPECT_CALL(mock, ReadRawData(testing::_, testing::_)).WillRepeatedly(Return(nullptr));
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest007
+ * @tc.desc: Function GetPasteData when Decode return error
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest007, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    std::vector<uint8_t> mockData = {0x01, 0x02, 0x03};
+    PasteData testData;
+    NiceMock<PasteboardDelayProxyInterfaceMock> mock;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    EXPECT_CALL(mock, WriteInterfaceToken(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, WriteString(testing::_)).WillOnce(Return(true));
+    EXPECT_CALL(mock, SendRequest(testing::_, testing::_, testing::_, testing::_))
+               .WillRepeatedly(Return(OHOS::ERR_OK));
+    EXPECT_CALL(mock, ReadInt64()).WillRepeatedly(Return(mockData.size()));
+    EXPECT_CALL(mock, ReadUnpadBuffer(testing::_)).WillRepeatedly(Return(mockData.data()));
+    //EXPECT_CALL(mock, ReadRawData(testing::_, testing::_)).WillRepeatedly(Return(mockData.data()));
+    EXPECT_CALL(mock, Decode(testing::_)).WillRepeatedly(Return(false));
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetPasteData(testType, testData));
+}
+
+/**
+ * @tc.name: GetPasteDataTest008
+ * @tc.desc: Function GetUnifiedData
+ * @tc.type: FUNC
+ */
+HWTEST_F(PasteboardDelayProxyTest, GetPasteDataTest008, TestSize.Level0)
+{
+    std::string testType = "text/plain";
+    UDMF::UnifiedData testData;
+    sptr<RemoteObjectTest> remote = new RemoteObjectTest(u"test");
+    PasteboardDelayGetterProxy proxy(remote);
+    ASSERT_NO_FATAL_FAILURE(proxy.GetUnifiedData(testType, testData));
+}
+}//namespace OHOS
