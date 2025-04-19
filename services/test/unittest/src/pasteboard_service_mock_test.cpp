3fefe2342635ad93c784d50252231581140cf2f0
王瑞峰 <wangruifeng14@huawei.com>
2025-04-19 08:16:50 +0000

    update services/test/unittest/src/pasteboard_service_mock_test.cpp.
    Signed-off-by: 王瑞峰 <wangruifeng14@huawei.com>

--- services/test/unittest/src/pasteboard_service_mock_test.cpp
+++ services/test/unittest/src/pasteboard_service_mock_test.cpp
@@ -1464,7 +1464,8 @@ HWTEST_F(PasteboardServiceTest, EstablishP2PLinkTest001, TestSize.Level0)
     std::string pasteld = "paste123";
     
     NiceMock<PasteboardServiceInterfaceMock> mock;
-    EXPECT_CALL(mock, GetRemoteDeviceInfo(testing::_, testing::_)).WillOnce(Return(static_cast<int32_t>(PasteboardError::E_OK));
+    EXPECT_CALL(mock, GetRemoteDeviceInfo(testing::_, testing::_))
+        .WillOnce(Return(static_cast<int32_t>(PasteboardError::E_OK));
     
     tempPasteboard->EstablishP2PLink(networkld, pasteld);
     ASSERT_TRUE(true);
@@ -1487,7 +1488,8 @@ HWTEST_F(PasteboardServiceTest, CloseP2PLinkTest001, TestSize.Level0)
     std::string networkld = "network123";
     
     NiceMock<PasteboardServiceInterfaceMock> mock;
-    EXPECT_CALL(mock, GetRemoteDeviceInfo(testing::_, testing::_)).WillOnce(Return(static_cast<int32_t>(PasteboardError::E_OK));
+    EXPECT_CALL(mock, GetRemoteDeviceInfo(testing::_, testing::_))
+        .WillOnce(Return(static_cast<int32_t>(PasteboardError::E_OK));
     
     tempPasteboard->CloseP2PLink(networkld);
 #else
