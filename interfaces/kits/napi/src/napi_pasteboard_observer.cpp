e749508f5a9c597f121c8d307d5096ce8949db8b
yangxiaodong41 <yangxiaodong41@huawei.com>
2025-10-20 14:36:44 +0800

    使用this保证task的生命周期内PasteboardObserverInstance对象始终有效
    Signed-off-by: yangxiaodong41 <yangxiaodong41@huawei.com>

--- interfaces/kits/napi/src/napi_pasteboard_observer.cpp
+++ interfaces/kits/napi/src/napi_pasteboard_observer.cpp
@@ -40,16 +40,16 @@ void PasteboardObserverInstance::OnPasteboardChanged()
 {
     pid_t threadId = syscall(SYS_gettid);
     PASTEBOARD_HILOGD(PASTEBOARD_MODULE_JS_NAPI, "enter");
-    auto task = [callback = callback_, threadId]() {
+    auto task = [this, threadId]() {
         PASTEBOARD_HILOGI(PASTEBOARD_MODULE_JS_NAPI, "napi_send_event start, originTid=%{public}d", threadId);
-        napi_status status = napi_acquire_threadsafe_function(callback);
+        napi_status status = napi_acquire_threadsafe_function(callback_);
         PASTEBOARD_CHECK_AND_RETURN_LOGE(status == napi_ok, PASTEBOARD_MODULE_JS_NAPI,
             "acquire callback failed, status=%{public}d", status);
-        status = napi_call_threadsafe_function(callback, nullptr, napi_tsfn_blocking);
+        status = napi_call_threadsafe_function(callback_, nullptr, napi_tsfn_blocking);
         if (status != napi_ok) {
             PASTEBOARD_HILOGE(PASTEBOARD_MODULE_JS_NAPI, "call callback failed, status=%{public}d", status);
         }
-        status = napi_release_threadsafe_function(callback, napi_tsfn_release);
+        status = napi_release_threadsafe_function(callback_, napi_tsfn_release);
         PASTEBOARD_CHECK_AND_RETURN_LOGE(status == napi_ok, PASTEBOARD_MODULE_JS_NAPI,
             "release callback failed, status=%{public}d", status);
     };
