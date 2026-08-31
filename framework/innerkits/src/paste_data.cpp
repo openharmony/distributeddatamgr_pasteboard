e29b974ab9b98394ea1eec9118c3c6da6e6ce106
yanghuiqing <yanghuiqing6@h-partners.com>
2026-08-31 10:17:54 +0800

    历史剪贴版扩大uri权限联合修改
    Co-Authored-By: Agent
    Signed-off-by: yanghuiqing <yanghuiqing6@h-partners.com>

--- framework/innerkits/src/paste_data.cpp
+++ framework/innerkits/src/paste_data.cpp
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2026-2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -386,7 +386,8 @@ int32_t PasteData::GetOriginTokenId()
 void PasteData::SetTokenId(uint32_t tokenId)
 { // LCOV_EXCL_START
     props_.tokenId = tokenId;
-    auto originInfo.SetParam(ORIGIN_TOKEN_ID, AAFwk::Integer::Box(tokenId));
+    auto originInfo = props_.additions.GetWantParams(ORIGIN_INFO);
+    originInfo.SetParam(ORIGIN_TOKEN_ID, AAFwk::Integer::Box(tokenId));
     props_.additions.SetParam(ORIGIN_INFO, AAFwk::WantParamWrapper::Box(std::move(originInfo)));
 } // LCOV_EXCL_STOP
 
